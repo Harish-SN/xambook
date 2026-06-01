ba00ede22792ddf2a771cbf4e3386b9030f1f4b6
Harish SN <snharish85@gmail.com>
2026-06-01 18:38:16 +0530

    edi payment

--- backend/db/postgres.go
+++ backend/db/postgres.go
@@ -12,18 +12,14 @@ var DB *sql.DB
 
 func Connect() {
 	var err error
-
 	DB, err = sql.Open("postgres", os.Getenv("POSTGRES_URL"))
 	if err != nil {
 		log.Fatal("Failed to connect to database:", err)
 	}
-
 	if err = DB.Ping(); err != nil {
 		log.Fatal("Database not reachable:", err)
 	}
-
 	log.Println("Connected to PostgreSQL")
-
 	createTables()
 }
 
